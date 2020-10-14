f58694165ead8b250b17de1848c1dc26e9d997a3
sathibca1990 <sathibca1990@gmail.com>
2020-10-14 11:06:23 +0530

    sample

---
+++ CISample/src/sample.java
@@ -0,0 +1,15 @@
+
+public class sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("selenium");
+     
+     System.out.println("jmeter");
+     
+     System.out.println("devops");
+     
+     System.out.println("AutoIT");
+	}
+
+}
